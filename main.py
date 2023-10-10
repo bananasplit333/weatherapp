5be0ca5b0a1620d3a31a66feedc102526e9fe228
bananasplit333 <134814180+bananasplit333@users.noreply.github.com>
2023-10-10 19:49:00 -0400

    add files

---
+++ main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from pprint import pprint as pp
+from flask import Flask, flash, redirect, render_template, request, url_for
+from weather import query_api
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('weather.html')
+
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    data = []
+    error = None
+    if request.method == 'POST':
+        city_name = request.form.get('user_input')
+        resp = query_api(city_name)
+        pp(resp)
+        if resp:
+            data.append(resp)
+        if len(data) != 2: 
+            error = 'Bad Response from Weather API'
+    return render_template('result.html', data=data, error=error)
+
+if __name__ == '__main__':
+    app.run(debug=True)
